66816ee9e05a1e80dd03eb360a8d38d9504398d8
James Lamb <jlamb@nvidia.com>
2024-10-02 10:34:47 -0700

    consolidate 'pip install' calls in CI scripts

--- ci/test_wheel_cugraph-dgl.sh
+++ ci/test_wheel_cugraph-dgl.sh
@@ -6,22 +6,15 @@ set -eoxu pipefail
 package_name="cugraph-dgl"
 package_dir="python/cugraph-dgl"
 
-python_package_name=$(echo ${package_name}|sed 's/-/_/g')
-
 mkdir -p ./dist
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 
 # Download wheels built during this job.
 RAPIDS_PY_WHEEL_NAME="pylibcugraph_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./local-deps
 RAPIDS_PY_WHEEL_NAME="cugraph_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./local-deps
-python -m pip install ./local-deps/*.whl
-
-# use 'ls' to expand wildcard before adding `[extra]` requires for pip
 RAPIDS_PY_WHEEL_NAME="${package_name}_${RAPIDS_PY_CUDA_SUFFIX}" RAPIDS_PY_WHEEL_PURE="1" rapids-download-wheels-from-s3 ./dist
-# pip creates wheels using python package names
-python -m pip install $(ls ./dist/${python_package_name}*.whl)[test]
 
-
+# determine which pytorch and DGL packages to install
 PKG_CUDA_VER="$(echo ${CUDA_VERSION} | cut -d '.' -f1,2 | tr -d '.')"
 PKG_CUDA_VER_MAJOR=${PKG_CUDA_VER:0:2}
 if [[ "${PKG_CUDA_VER_MAJOR}" == "12" ]]; then
@@ -32,8 +25,13 @@ fi
 PYTORCH_URL="https://download.pytorch.org/whl/cu${PYTORCH_CUDA_VER}"
 DGL_URL="https://data.dgl.ai/wheels/cu${PYTORCH_CUDA_VER}/repo.html"
 
-rapids-logger "Installing PyTorch and DGL"
-rapids-retry python -m pip install torch==2.3.0 --index-url ${PYTORCH_URL}
-rapids-retry python -m pip install dgl==2.0.0 --find-links ${DGL_URL}
+# echo to expand wildcard before adding `[test]` requires for pip
+python -m pip install \
+    --extra-index-url ${PYTORCH_URL} \
+    --find-links ${DGL_URL} \
+    "$(echo ./dist/cugraph_dgl_*.whl)[test]" \
+    ./local-deps/*.whl \
+    'dgl==2.0.0' \
+    'torch==2.3.0'
 
 python -m pytest python/cugraph-dgl/tests
--- ci/test_wheel_cugraph-equivariant.sh
+++ ci/test_wheel_cugraph-equivariant.sh
@@ -6,17 +6,13 @@ set -eoxu pipefail
 package_name="cugraph-equivariant"
 package_dir="python/cugraph-equivariant"
 
-python_package_name=$(echo ${package_name}|sed 's/-/_/g')
-
 mkdir -p ./dist
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 
-# use 'ls' to expand wildcard before adding `[extra]` requires for pip
+# Download wheels built during this job.
 RAPIDS_PY_WHEEL_NAME="${package_name}_${RAPIDS_PY_CUDA_SUFFIX}" RAPIDS_PY_WHEEL_PURE="1" rapids-download-wheels-from-s3 ./dist
-# pip creates wheels using python package names
-python -m pip install $(ls ./dist/${python_package_name}*.whl)[test]
 
-
+# determine which pytorch and DGL packages to install
 PKG_CUDA_VER="$(echo ${CUDA_VERSION} | cut -d '.' -f1,2 | tr -d '.')"
 PKG_CUDA_VER_MAJOR=${PKG_CUDA_VER:0:2}
 if [[ "${PKG_CUDA_VER_MAJOR}" == "12" ]]; then
@@ -26,8 +22,12 @@ else
 fi
 PYTORCH_URL="https://download.pytorch.org/whl/cu${PYTORCH_CUDA_VER}"
 
-rapids-logger "Installing PyTorch and e3nn"
-rapids-retry python -m pip install torch --index-url ${PYTORCH_URL}
-rapids-retry python -m pip install e3nn
+# echo to expand wildcard before adding `[test]` requires for pip
+python -m pip install \
+    --extra-index-url ${PYTORCH_URL} \
+    "$(echo ./dist/cugraph_equivariant_*.whl)[test]" \
+    ./local-deps/*.whl \
+    'e3nn' \
+    'torch==2.3.0'
 
 python -m pytest python/cugraph-equivariant/cugraph_equivariant/tests
